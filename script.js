d8cda7a58d4cc32bf45e1d93cfdda04d29fe3a11
Parami Books Editor <editor@paramibooks.local>
2026-08-14 22:42:33 +0200

    Update Magia Cozy Spanish cover image to new design (v3.1)

--- script.js
+++ script.js
@@ -203,13 +203,13 @@ function renderCatalog(market) {
     
     let coverUrl = "";
     if (details.coverImage) {
-      coverUrl = `images/${details.coverImage}?v=3.0`;
+      coverUrl = `images/${details.coverImage}?v=3.1`;
     } else if (book.coverImage) {
-      coverUrl = `images/${book.coverImage}?v=3.0`;
+      coverUrl = `images/${book.coverImage}?v=3.1`;
     } else if (asin && !details.comingSoon) {
       coverUrl = `https://images-na.ssl-images-amazon.com/images/P/${asin}.01.LZZZZZZZ.jpg`;
     } else {
-      coverUrl = `images/${prefix}_cover_${currentLanguage}.png?v=3.0`;
+      coverUrl = `images/${prefix}_cover_${currentLanguage}.png?v=3.1`;
     }
 
     const card = document.createElement("div");
@@ -252,13 +252,13 @@ function renderCatalog(market) {
         <div class="carousel-track" style="transform: translateX(0%);">
           ${firstSlideHtml}
           <div class="carousel-slide">
-            <img class="book-cover-img" src="images/${prefix}_page_1.png?v=3.0" alt="Página para colorear 1" onerror="this.src='images/mandalas_page_1.png?v=3.0'">
+            <img class="book-cover-img" src="images/${prefix}_page_1.png?v=3.1" alt="Página para colorear 1" onerror="this.src='images/mandalas_page_1.png?v=3.1'">
           </div>
           <div class="carousel-slide">
-            <img class="book-cover-img" src="images/${prefix}_page_2.png?v=3.0" alt="Página para colorear 2" onerror="this.src='images/mandalas_page_2.png?v=3.0'">
+            <img class="book-cover-img" src="images/${prefix}_page_2.png?v=3.1" alt="Página para colorear 2" onerror="this.src='images/mandalas_page_2.png?v=3.1'">
           </div>
           <div class="carousel-slide">
-            <img class="book-cover-img" src="images/${prefix}_page_3.png?v=3.0" alt="Página para colorear 3" onerror="this.src='images/mandalas_page_3.png?v=3.0'">
+            <img class="book-cover-img" src="images/${prefix}_page_3.png?v=3.1" alt="Página para colorear 3" onerror="this.src='images/mandalas_page_3.png?v=3.1'">
           </div>
         </div>
 
