ac1eec994031ca47d18dcda2fb03b1831dc51c3b
Parami Books Editor <editor@paramibooks.local>
2026-08-24 01:05:54 +0200

    Fix file mapping: assign Gecko title cover image to alma_salvaje_cover_es.png v6.0

--- script.js
+++ script.js
@@ -203,11 +203,11 @@ function renderCatalog(market) {
     
     let coverUrl = "";
     if (details.coverImage) {
-      coverUrl = `images/${details.coverImage}?v=5.6`;
+      coverUrl = `images/${details.coverImage}?v=6.0`;
     } else if (book.coverImage) {
-      coverUrl = `images/${book.coverImage}?v=5.6`;
+      coverUrl = `images/${book.coverImage}?v=6.0`;
     } else {
-      coverUrl = `images/${prefix}_cover_${currentLanguage}.png?v=5.6`;
+      coverUrl = `images/${prefix}_cover_${currentLanguage}.png?v=6.0`;
     }
 
     const card = document.createElement("div");
@@ -250,13 +250,13 @@ function renderCatalog(market) {
         <div class="carousel-track" style="transform: translateX(0%);">
           ${firstSlideHtml}
           <div class="carousel-slide">
-            <img class="book-cover-img" src="images/${prefix}_page_1.png?v=5.6" alt="Página para colorear 1" onerror="this.src='images/mandalas_page_1.png?v=5.6'">
+            <img class="book-cover-img" src="images/${prefix}_page_1.png?v=6.0" alt="Página para colorear 1" onerror="this.src='images/mandalas_page_1.png?v=6.0'">
           </div>
           <div class="carousel-slide">
-            <img class="book-cover-img" src="images/${prefix}_page_2.png?v=5.6" alt="Página para colorear 2" onerror="this.src='images/mandalas_page_2.png?v=5.6'">
+            <img class="book-cover-img" src="images/${prefix}_page_2.png?v=6.0" alt="Página para colorear 2" onerror="this.src='images/mandalas_page_2.png?v=6.0'">
           </div>
           <div class="carousel-slide">
-            <img class="book-cover-img" src="images/${prefix}_page_3.png?v=5.6" alt="Página para colorear 3" onerror="this.src='images/mandalas_page_3.png?v=5.6'">
+            <img class="book-cover-img" src="images/${prefix}_page_3.png?v=6.0" alt="Página para colorear 3" onerror="this.src='images/mandalas_page_3.png?v=6.0'">
           </div>
         </div>
 
